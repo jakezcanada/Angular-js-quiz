5c9f6bb443194e4b5dd16915221b4fe6a6f2d174
Jake <gmhelper@gmail.com>
2022-08-15 16:22:28 -0400

    Fix compilation error

--- ui/src/app/leaderboard/leaderboard.component.ts
+++ ui/src/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Leaderboard} from "../leaderboard";
+import {NgForm} from "@angular/forms";
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-leaderboard',
@@ -6,10 +9,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./leaderboard.component.scss']
 })
 export class LeaderboardComponent implements OnInit {
+  selectedLeaderboard: Leaderboard | undefined;
+  leaderboards: Leaderboard[] = [
+    {
+      id: 1,
+      title: 'Test Leaderboard1',
+      players: [
+        {name: 'test1', score: 1},
+        {name: 'test2', score: 3},
+        {name: 'test5', score: 6},
+      ]
+    },
+    {
+      id: 2,
+      title: 'Test Leaderboard2',
+      players: [
+        {name: 'test1', score: 1},
+        {name: 'test2', score: 3},
+        {name: 'test5', score: 6},
+      ]
+    },
+    {
+      id: 3,
+      title: 'Test Leaderboard3',
+      players: [
+        {name: 'test1', score: 1},
+        {name: 'test2', score: 3},
+        {name: 'test5', score: 6},
+      ]
+    }
+  ];
+
+  playerNameInput: string | undefined;
+  playerScoreInput: number | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectLeaderboard(leaderboard: Leaderboard){
+    this.selectedLeaderboard = leaderboard;
+  }
+
+  onReturnToLeaderboardList(){
+    this.selectedLeaderboard = undefined;
+  }
+
+  onAddPlayer(f: NgForm){
+    this.selectedLeaderboard?.players.push(
+      {
+        name: f.value.name,
+        score: f.value.score
+      }
+    );
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.leaderboards, event.previousIndex, event.currentIndex);
+  }
+
+  onCreateNewLeaderboard() {
+
   }
 
 }
